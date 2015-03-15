28d13bafcf07b04e03e3424c5fb57a52472962e1
piglikeyoung <316046510@qq.com>
2015-03-15 15:26:21 +0800

    Initial Commit

---
+++ 画画板/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  画画板
+//
+//  Created by piglikeyoung on 15/3/15.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 画画板/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  画画板
+//
+//  Created by piglikeyoung on 15/3/15.
+//  Copyright (c) 2015年 jinheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
